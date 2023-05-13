57a355f18341044756593eb671c76c2f300c737e
mylcevaleksandr <myltsevaleksandr@gmail.com>
2023-05-14 01:39:30 +0300

    Finished final layout of quiz site

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,52 @@ gulp.task( "html", () => {
 
 // собирает все файлы less в один css, минифицирует его и добавляет .min к названию.
 gulp.task( "less", () => {
-    return gulp.src( "./src/Styles/*.less" )
+    return gulp.src( "./src/Styles/index.less" )
         .pipe( less( {
             plugins: [ autoPreFix ]
         } ) )
-        .pipe( concatCss( "style.css" ) )
+        .pipe( concatCss( "index.css" ) )
+        .pipe( cssMin() )
+        .pipe( rename( { suffix: ".min" } ) )
+        .pipe( gulp.dest( "docs/Styles" ) );
+} );
+gulp.task( "lessTwo", () => {
+    return gulp.src( "./src/Styles/form.less" )
+        .pipe( less( {
+            plugins: [ autoPreFix ]
+        } ) )
+        .pipe( concatCss( "form.css" ) )
+        .pipe( cssMin() )
+        .pipe( rename( { suffix: ".min" } ) )
+        .pipe( gulp.dest( "docs/Styles" ) );
+} );
+
+gulp.task( "lessThree", () => {
+    return gulp.src( "./src/Styles/choice.less" )
+        .pipe( less( {
+            plugins: [ autoPreFix ]
+        } ) )
+        .pipe( concatCss( "choice.css" ) )
+        .pipe( cssMin() )
+        .pipe( rename( { suffix: ".min" } ) )
+        .pipe( gulp.dest( "docs/Styles" ) );
+} );
+gulp.task( "lessFour", () => {
+    return gulp.src( "./src/Styles/test.less" )
+        .pipe( less( {
+            plugins: [ autoPreFix ]
+        } ) )
+        .pipe( concatCss( "test.css" ) )
+        .pipe( cssMin() )
+        .pipe( rename( { suffix: ".min" } ) )
+        .pipe( gulp.dest( "docs/Styles" ) );
+} );
+gulp.task( "lessFive", () => {
+    return gulp.src( "./src/Styles/result.less" )
+        .pipe( less( {
+            plugins: [ autoPreFix ]
+        } ) )
+        .pipe( concatCss( "result.css" ) )
         .pipe( cssMin() )
         .pipe( rename( { suffix: ".min" } ) )
         .pipe( gulp.dest( "docs/Styles" ) );
@@ -52,7 +93,8 @@ gulp.task( "browser-sync", function () {
 gulp.task( "watch", () => {
     gulp.watch( "src/*.html" ).on( "change", gulp.parallel( "html" ) );
     gulp.watch( "src/JS/*.js" ).on( "change", gulp.parallel( "jsMin" ) );
-    gulp.watch( "src/Styles/*.less" ).on( "change", gulp.parallel( "less" ) );
+    gulp.watch( "src/Styles/*.less" ).on( "change", gulp.parallel( ["lessFive","less","lessTwo","lessThree","lessFour"] ) );
+
 } );
 
 gulp.task( "default", gulp.parallel(
