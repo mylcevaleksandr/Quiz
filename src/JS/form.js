8b31325871e81e4ac5d1dffcd216b42e8581257e
mylcevaleksandr <myltsevaleksandr@gmail.com>
2025-03-24 21:59:43 +0300

    Fix login form validation

--- src/JS/form.js
+++ src/JS/form.js
@@ -6,6 +6,7 @@
             {
                 name: "name",
                 id: "name",
+                label: "Имя",
                 element: null,
                 regex: /^[А-Я][а-я]+\s*$/,
                 valid: false,
@@ -13,6 +14,7 @@
             {
                 name: "lastName",
                 id: "last-name",
+                label: "Фамилия",
                 element: null,
                 regex: /^[А-Я][а-я]+\s*$/,
                 valid: false,
@@ -20,6 +22,7 @@
             {
                 name: "email",
                 id: "email",
+                label: "Электронная почта",
                 element: null,
                 regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
                 valid: false,
@@ -27,52 +30,54 @@
         ],
         init() {
             const that = this;
-            this.fields.forEach( item => {
-                item.element = document.getElementById( item.id );
+            this.fields.forEach(item => {
+                item.element = document.getElementById(item.id);
                 item.element.onchange = function () {
-                    that.validateField.call( that, item, this );
+                    that.validateField.call(that, item, this);
                 };
-            } );
-            this.processElement = document.getElementById( "process" );
+            });
+            this.processElement = document.getElementById("process");
             this.processElement.onclick = function () {
                 that.processForm();
             };
-            this.agreeElement = document.getElementById( "agree" );
+            this.agreeElement = document.getElementById("agree");
             this.agreeElement.onchange = function () {
                 that.validateForm();
             };
         },
-        validateField( field, element ) {
-            if ( !element.value || !element.value.match( field.regex ) ) {
-                element.parentNode.style.borderColor = "red";
+        validateField(field, element) {
+            const parent = element.parentNode;
+            const labelElement = parent.children[1];
+            if (!element.value || !element.value.match(field.regex)) {
+                parent.style.borderColor = "red";
+                labelElement.innerText = labelElement.innerText + " Русскими буквами!";
                 field.valid = false;
-            }
-            else {
-                element.parentNode.removeAttribute( "style" );
+            } else {
+                labelElement.innerText = field.label;
+                parent.removeAttribute("style");
                 field.valid = true;
             }
             this.validateForm();
         },
         validateForm() {
 
-            const validForm = this.fields.every( item => item.valid );
+            const validForm = this.fields.every(item => item.valid);
             const isValid = this.agreeElement.checked && validForm;
-            if ( isValid ) {
-                this.processElement.removeAttribute( "disabled" );
-            }
-            else {
-                this.processElement.setAttribute( "disabled", "disabled" );
+            if (isValid) {
+                this.processElement.removeAttribute("disabled");
+            } else {
+                this.processElement.setAttribute("disabled", "disabled");
             }
             return isValid;
         },
         processForm() {
-            if ( this.validateForm() ) {
+            if (this.validateForm()) {
                 let paramString = "";
-                this.fields.forEach( item => {
-                    sessionStorage.setItem( item.name, item.element.value );
+                this.fields.forEach(item => {
+                    sessionStorage.setItem(item.name, item.element.value);
 
                     paramString += (!paramString ? "?" : "&") + item.name + "=" + item.element.value;
-                } );
+                });
 
 
                 location.href = "choice.html";
